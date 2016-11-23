8f91c2872345c286b66f53141dd2efa23474d81e
Bryan <begos@suitedbpo.com>
2016-11-23 18:33:43 +0800

    added back top button

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,38 @@
 
-$('.col-icon-a').fadeOut(2000);
-
 $(function(){
     $(".fa-arrow-circle-down").click(function(){
         $(this).fadeOut(2000).fadeIn(2000);
     });
-
     $(".fa-wordpress").click(function(){
         $(this).fadeOut(2000).fadeIn(2000);
     });
-
     $(".fa-arrow-circle-down").click(function(){
         $('body').animate({ scrollTop: 0 }, "slow");
     });
-
-    $(".fa-arrow-circle-up").click(function(){
+    $(".glyphicon-menu-up").click(function(){
         $('body').animate({ scrollTop: 0 }, "slow");
-        $(this).fadeOut(2000).fadeIn(2000);
     });
 });
 
 $(window).scroll(function() {
-    if ($(this).scrollTop()>0)
+    if ($(this).scrollTop() ==0)
      {
-        $('.header-top-bg').fadeOut(1000);
+        $('.glyphicon-menu-up').fadeOut(1000);
+    } else {
+        $('.glyphicon-menu-up').fadeIn(1000);
+    }
+    if ($(this).scrollTop() >0)
+     {
+        $('.header-top-bg').fadeIn(1000);
+        $('.header-top-bg').css("background", "rgba(2, 2, 2, 0.29)");
+        $('.header-top-bg').css("padding-top", "0px");
     }
     else
      {
-      $('.header-top-bg').fadeIn(1000);
+        $('.header-top-bg').fadeOut(1000);
+        $('.header-top-bg').fadeIn(1000);
+        $('.header-top-bg').css("background", "transparent");
+        $('.header-top-bg').css("padding-top", "50px");
+
      }
  });
-
- $(window).scroll(function() {
-     if ($(this).scrollTop()>200)
-      {
-         $('.text-content').fadeOut(1000);
-     }
-     else
-      {
-       $('.text-content').fadeIn(1000);
-      }
-  });
